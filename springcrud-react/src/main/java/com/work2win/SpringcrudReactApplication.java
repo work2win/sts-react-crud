395c3687093b91281af7b438b40f0f2694e46e81
work2win <worknpeace2win@gmail.com>
2023-04-13 16:52:23 -0400

    intial commit sts

---
+++ springcrud-react/src/main/java/com/work2win/SpringcrudReactApplication.java
@@ -0,0 +1,13 @@
+package com.work2win;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcrudReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcrudReactApplication.class, args);
+	}
+
+}
---
+++ springcrud-react/src/test/java/com/work2win/SpringcrudReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work2win;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcrudReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
